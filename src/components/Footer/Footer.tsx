e8e24c37039f39c80332dc56b990c3ecaf689286
Oscar Salas <estebansalas2502@gmail.com>
2024-03-04 09:17:44 -0600

    Cambios

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,6 +3,7 @@ import './Footer.scss'
 export const Footer:React.FC= () => {
     return (
              <footer className="content_footer">
+                <p>Hecho por Oscar Salas Miranda</p>
              </footer>
     );
   }
